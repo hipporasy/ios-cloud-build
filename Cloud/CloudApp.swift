45a09e2d298fd7722aa9efb3af3f35e930fed26c
hipporasy <marasy.phi@bilue.com.au>
2023-03-27 09:25:36 +1100

    Initial commit

---
+++ Cloud/CloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudApp.swift
+//  Cloud
+//
+//  Created by Rasy on 27/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct CloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
